e0dc045c30fe77d14dcee6a83a3117bc810346f3
Nazmul8536 <nazmul.cse2019@gmail.com>
2024-08-27 13:01:32 +0600

    post

---
+++ components/PostUser.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="relative brightness-90 hover:brightness-[1.1] cursor-pointer"
+    @mouseenter="($event) => isHover(true)"
+    @mouseleave="($event) => isHover(false)"
+  >
+    <div
+      v-if="!isLoaded"
+      class="absolute flex items-center justify-center top-0 left-0 aspect-[3/4] w-full object-cover rounded-md bg-black"
+    >
+      <Icon
+        name="mingcute:loading-line"
+        size="100"
+        class="ml-1 animate-spin"
+        style="color: aliceblue"
+      />
+    </div>
+
+    <div>
+      <video
+        class="aspect-[3/4] object-cover rounded-md"
+        ref="video"
+        muted
+        loop
+        src="../public/223928_small.mp4"
+      />
+    </div>
+
+    <div class="px-1">
+      <div class="text-gray-700 text-[15px] pt-1 font-bold text-xs">
+        This is Some Text
+      </div>
+      <div class="flex items-center -ml-1 text-xs font-bold text-gray-600">
+        <Icon name="gg:loadbar-sound" size="20" />3%
+        <Icon name="tabler:alert-circle" class="ml-1" size="16" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps(["post"]);
+
+const route = ref(null);
+const router = ref(false);
+
+let video = ref(null);
+let isLoaded = ref(false);
+
+onMounted(() => {
+  if (video.value) {
+    video.value.addEventListener("loadeddata", (e) => {
+      if (e.target) {
+        setTimeout(() => {
+          isLoaded.value = true;
+        }, 200);
+      }
+    });
+  }
+});
+
+onBeforeUnmount(() => {
+  video.value.pause();
+  video.value.currentTime = 0;
+  video.value.src = "";
+});
+
+const isHover = (bool) => {
+  if (bool) {
+    video.value.play();
+  } else {
+    video.value.pause();
+  }
+};
+</script>
---
+++ pages/profile/[id].vue
@@ -0,0 +1,78 @@
+<template lang="">
+  <MainLayouts>
+    <div
+      class="pt-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 pr-2 w-[calc(100%-90px)] max-w-[1800px] 2xl:mx-auto"
+    >
+      <div class="flex w-[calc(100vw-230px)]">
+        <img
+          class="max-w-[120px] rounded-full"
+          src="https://picsum.photos/id/8/300/320"
+        />
+
+        <div class="w-full ml-5">
+          <div class="text-[30px] font-bold truncate">User Name</div>
+          <div class="text-[18px] truncate">User Name</div>
+          <button
+            v-if="true"
+            class="flex rounded-md item-center py-1.5 px-3.5 mt-3 text-[15px] font-semibold border hover:bg-gray-100"
+          >
+            <Icon name="mdi:pencil" size="18" class="mt-0.5 mr-1" />
+            <div>Edit Profile</div>
+          </button>
+          <button
+            v-else
+            class="flex items-center rounded-md py-1.5 px-8 mt-3 text-[15px] font-semibold border bg-[#f02c56] text-white"
+          >
+            Follow
+          </button>
+        </div>
+      </div>
+      <div class="flex items-center pt-4">
+        <div class="mr-4">
+          <span class="font-bold">10K</span>
+          <span class="font-light text-gray-500 text-[15px] pl-1.5"
+            >Following</span
+          >
+        </div>
+        <div class="mr-4">
+          <span class="font-bold">40K</span>
+          <span class="font-light text-gray-500 text-[15px] pl-1.5"
+            >Followers</span
+          >
+        </div>
+        <div class="mr-4">
+          <span class="font-bold">44K</span>
+          <span class="font-light text-gray-500 text-[15px] pl-1.5">Likes</span>
+        </div>
+      </div>
+      <div
+        class="pt-4 mr-4 font-light text-gray-500 text-[15px] pl-1.5 max-w-[500px]"
+      >
+        This is Bio Section
+      </div>
+      <div class="flex items-center w-full pt-4 border-b">
+        <div
+          class="py-2 text-center w-60 text-[17px] font-semibold border-b-2 border-b-black"
+        >
+          Videos
+        </div>
+        <div
+          class="py-2 text-center text-gray-500 w-60 text-[17px] font-semibold"
+        >
+          <Icon name="material-symbols:lock-open" class="" />Liked
+        </div>
+      </div>
+
+      <div
+        class="grid grid-cols-2 gap-2 mt-4 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3"
+      >
+        <PostUser />
+        <PostUser />
+      </div>
+    </div>
+  </MainLayouts>
+</template>
+<script setup>
+import MainLayouts from "~/layouts/MainLayouts.vue";
+import PostUser from "~/components/PostUser.vue";
+</script>

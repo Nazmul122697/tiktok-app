d0fb536089e72d69ec1699579d9ca7ca19aa4394
Nazmul8536 <nazmul.cse2019@gmail.com>
2024-08-29 17:42:21 +0600

    input

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -103,52 +103,93 @@
 
       <div class="flex items-center px-8 mt-8">
         <div class="flex items-center pb-4 text-center">
-            <button class="p-2 bg-gray-200 rounded-full cursor-pointer">
-                <Icon name="mingcute:heart-fill" size="25" />
-            </button>
+          <button class="p-2 bg-gray-200 rounded-full cursor-pointer">
+            <Icon name="mingcute:heart-fill" size="25" />
+          </button>
         </div>
-        <div class="pl-2 pr-4 text-xs font-semibold text-gray-800">
-            123
-        </div>
+        <div class="pl-2 pr-4 text-xs font-semibold text-gray-800">123</div>
         <div class="flex items-center pb-4 text-center">
-            <div class="p-2 bg-gray-200 rounded-full cursor-pointer">
-                <Icon name="mingcute:message-3-fill" size="25" />
-            </div>
+          <div class="p-2 bg-gray-200 rounded-full cursor-pointer">
+            <Icon name="mingcute:message-3-fill" size="25" />
+          </div>
         </div>
-        <div class="pl-2 pr-4 text-xs font-semibold text-gray-800">
-            123
-        </div>
+        <div class="pl-2 pr-4 text-xs font-semibold text-gray-800">123</div>
       </div>
-      
-      <div id="comments" class="bg-[#F8f8f8] z-0 h-[calc(100%-273px)] border-t-2 overflow-auto">
+
+      <div
+        id="comments"
+        class="bg-[#F8f8f8] z-0 h-[calc(100%-273px)] border-t-2 overflow-auto"
+      >
         <div class="pt-2" />
         <div v-if="false" class="mt-6 text-xl text-center text-gray-500">
-            No comments
-            
+          No comments
         </div>
-        
+
         <div v-else class="relative flex justify-between px-8 mt-4">
-            <div class="relative flex items-center w-full">
-                <nuxt-link to="">
-                    <img class="absolute top-0 mx-auto rounded-full lg:mx-0" width="40" 
-                    src="https://picsum.photos/id/8/300/320"
-                    />
-                </nuxt-link>
-                <div class="ml-14 pt-0.5 w-full">
-                    <div class="text-[18px] font-semibold flex items-center justify-between">
-                        User Name
-                        <Icon v-if="true"
-                        @click="$event => deleteComment()"
-                        class="cursor-pointer"
-                        name="material-symbols:delete-outline-sharp"
-                        size="25"
-                        />
-                    </div>
-                </div>
+          <div class="relative flex items-center w-full">
+            <nuxt-link to="">
+              <img
+                class="absolute top-0 mx-auto rounded-full lg:mx-0"
+                width="40"
+                src="https://picsum.photos/id/8/300/320"
+              />
+            </nuxt-link>
+            <div class="ml-14 pt-0.5 w-full">
+              <div
+                class="text-[18px] font-semibold flex items-center justify-between"
+              >
+                User Name
+                <Icon
+                  v-if="true"
+                  @click="($event) => deleteComment()"
+                  class="cursor-pointer"
+                  name="material-symbols:delete-outline-sharp"
+                  size="25"
+                />
+              </div>
+
+              <div class="text-[15px] font-light">
+                Ea et aliqua cillum in reprehenderit occaecat occaecat irure
+                occaecat. Deserunt sit velit ipsum dolor qui amet commodo veniam
+                proident et eiusmod. Consectetur aute eiusmod labore veniam.
+                Cupidatat eu dolore velit qui occaecat irure id laboris sunt sit
+                in sit exercitation culpa. Id reprehenderit minim reprehenderit
+                in est duis et ad occaecat et occaecat.
+              </div>
             </div>
+          </div>
         </div>
+        <div class="mb-28" />
+      </div>
+
+      <div
+        id="CreateComment"
+        v-if="true"
+        class="absolute bottom-0 flex items-center justify-between lg:max-w-[550px] bg-white h-[85px] w-full py-5 px-8 border-t-2"
+      >
+        <div
+          :class="
+            inputFocused
+              ? 'border-2 border-gray-400'
+              : 'border-2 border-[#F1f1f2]'
+          "
+          class="bg-[#f1f1f2] flex items-center rounded-lg w-full lg:max-w-[420px]"
+        >
+          <input
+            v-model="comment"
+            @focus="($event) => (inputFocused = true)"
+            @blur="($event) => (inputFocused = false)"
+            class="bg-[#f1f1f2] text-[14px] focus:ouline-none w-full lg:max-w-[420px] p-2 rounded-lg"
+            type="text"
+            placeholder="Add Comment ......."
+          />
+        </div>
+        <button>Post</button>
       </div>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+let inputFocused = ref(false);
+let comment = ref(null);
+</script>
